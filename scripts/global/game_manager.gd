a1f59deebd34db9654f012b3855b1f021d342f7d
Lertos <deetasticgaming@gmail.com>
2023-10-08 20:14:11 -0300

    Adding more skins

--- scripts/global/game_manager.gd
+++ scripts/global/game_manager.gd
@@ -3,7 +3,14 @@ extends Node
 var skins = [
 	preload("res://assets/player.png"),
 	preload("res://assets/player2.png"),
-	preload("res://assets/player3.png")
+	preload("res://assets/player3.png"),
+	preload("res://assets/player4.png"),
+	preload("res://assets/player5.png"),
+	preload("res://assets/player6.png"),
+	preload("res://assets/player7.png"),
+	preload("res://assets/player8.png"),
+	preload("res://assets/player9.png"),
+	preload("res://assets/player10.png")
 ]
 
 var players = {}
