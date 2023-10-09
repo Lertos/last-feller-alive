22da5a7ebbe006114bb32a006a6059207e69aa45
Lertos <deetasticgaming@gmail.com>
2023-10-08 23:55:31 -0300

    Using a better image; uses aliasing

--- gravity_field.gd
+++ gravity_field.gd
@@ -8,7 +8,7 @@ extends Node2D
 
 func _ready():
 	var tween_bg = $BG.create_tween().set_loops()
-	tween_bg.tween_property($BG, "rotation", TAU, 3).as_relative()
+	tween_bg.tween_property($BG, "rotation", TAU, 0.5).as_relative()
 	
 	var tween_fg = $FG.create_tween().set_loops()
 	tween_fg.tween_property($FG, "rotation", TAU, 1).as_relative()
