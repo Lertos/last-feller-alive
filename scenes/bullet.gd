bd48a10f31e978a300b8ada18f7d41bf9705637e
Lertos <deetasticgaming@gmail.com>
2023-10-09 13:22:10 -0300

    Fixing bullets as they were colliding badly

--- scenes/bullet.gd
+++ scenes/bullet.gd
@@ -1,19 +1,23 @@
-extends CharacterBody2D
+extends Node2D
 
+var vel = 0
 var speed = 400
 var is_active = false
-var direction: Vector2
 
 
 func _process(delta):
 	if is_active:
-		velocity = direction * speed
-		
-		if move_and_slide():
-			queue_free()
+		translate(vel * delta)
 
 
 func activate(position_to_target: Vector2):
-	direction = global_position.direction_to(position_to_target)
-
+	var direction = global_position.direction_to(position_to_target)
+	vel = direction * speed
+	
 	is_active = true
+
+
+func _on_area_2d_body_entered(body):
+	if body.is_in_group("player"):
+		if not body.is_dashing:
+			queue_free()
