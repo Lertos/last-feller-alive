8b7e5b2193f94b2400721ab6d300aed0fc755ceb
Lertos <deetasticgaming@gmail.com>
2023-10-13 17:48:19 -0300

    Adding blast damage from bombs

--- scenes/bomb.gd
+++ scenes/bomb.gd
@@ -3,8 +3,11 @@ extends Node2D
 @export var prime_loop_anim_time: float = 1.2
 @export var prime_count_before_explode: int = 3
 
-@export var min_scale: float = 2.0
-@export var max_scale: float = 2.3
+@export var min_bomb_scale: float = 1.8
+@export var max_bomb_scale: float = 2.3
+@export var max_circle_scale: float = 4
+
+var bomb_damage: float = 30.0
 
 
 func _ready():
@@ -19,14 +22,14 @@ func _ready():
 	timer.start()
 	
 	var tween_circle_scale = $Circle.create_tween()
-	tween_circle_scale.tween_property($Circle, "scale", Vector2(max_scale, max_scale),time_to_explode)
+	tween_circle_scale.tween_property($Circle, "scale", Vector2(max_circle_scale, max_circle_scale),time_to_explode)
 
 	var tween_circle_modulate = $Circle.create_tween()
-	tween_circle_modulate.tween_property($Circle, "modulate", Color.html("#ffffff8c"), time_to_explode)
+	tween_circle_modulate.tween_property($Circle/Circle, "modulate", Color.html("#ffffff8c"), time_to_explode)
 
 	var tween_bomb_scale = $Bomb.create_tween().set_loops()
-	tween_bomb_scale.tween_property($Bomb, "scale", Vector2(max_scale, max_scale), half_prime_anim_time)
-	tween_bomb_scale.tween_property($Bomb, "scale", Vector2(min_scale, min_scale), half_prime_anim_time)
+	tween_bomb_scale.tween_property($Bomb, "scale", Vector2(max_bomb_scale, max_bomb_scale), half_prime_anim_time)
+	tween_bomb_scale.tween_property($Bomb, "scale", Vector2(min_bomb_scale, min_bomb_scale), half_prime_anim_time)
 	
 	var tween_bomb_modulate = $Bomb.create_tween().set_loops()
 	tween_bomb_modulate.tween_property($Bomb, "modulate", Color.html("#d72400"), half_prime_anim_time)
@@ -37,6 +40,12 @@ func on_explosion():
 	#Hide the current sprites
 	$Circle.visible = false
 	$Bomb.visible = false
+	
+	#Check for players inside the blast area
+	for body in $Circle.get_overlapping_bodies():
+		if body.is_in_group("player"):
+			if not body.is_dashing and not body.is_dead:
+				body.add_health(-bomb_damage)
 	
 	#Handle the explosion
 	var explosion = $Explosion
