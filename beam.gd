88b21b66443cab687541307658839795edbeb327
Lertos <deetasticgaming@gmail.com>
2023-10-14 16:55:48 -0300

    Adding beam logic to grow slowly and then flash

--- beam.gd
+++ beam.gd
@@ -4,7 +4,7 @@ var rot_angle: float
 var start_scale: float = 0
 var beam_length_scale : float
 var beam_width_scale: float = 1.5
-var beam_width_active_scale: float = 3.5
+var beam_width_active_scale: float = 10.0
 
 
 func _ready():
@@ -14,4 +14,9 @@ func _ready():
 	beam_length_scale = max(Config.arena_width, Config.arena_height) / 2
 	
 	rotation = rot_angle
-	scale = Vector2(1, beam_length_scale)
+	scale = Vector2(0, beam_length_scale)
+
+	var tween_scale = create_tween()
+	tween_scale.tween_property(self, "scale", Vector2(beam_width_scale, beam_length_scale), 2.0)
+	tween_scale.tween_property(self, "scale", Vector2(beam_width_active_scale, beam_length_scale), 0.1)
+	tween_scale.tween_property(self, "scale", Vector2(0, beam_length_scale), 0.1)
