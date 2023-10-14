9aff78a6bd0e4799a2afaf9ca719851928e498a6
Lertos <deetasticgaming@gmail.com>
2023-10-14 17:16:57 -0300

    Making script more efficient and clean

--- beam.gd
+++ beam.gd
@@ -1,8 +1,5 @@
 extends Node2D
 
-var rot_angle: float
-var start_scale: float = 0
-var beam_length_scale : float
 var beam_width_scale: float = 1.5
 var beam_width_active_scale: float = 10.0
 
@@ -10,13 +7,16 @@ var beam_width_active_scale: float = 10.0
 func _ready():
 	var rng = RandomNumberGenerator.new()
 	
-	rot_angle = rng.randf_range(0.0, 359.9)
-	beam_length_scale = max(Config.arena_width, Config.arena_height) / 2
+	var startup_time: float = 2.0
+	var active_time: float = 0.1
+	var disappear_time: float = 0.1
 	
-	rotation = rot_angle
+	var beam_length_scale : float = max(Config.arena_width, Config.arena_height) / 2
+	
+	rotation = rng.randf_range(0.0, 359.9)
 	scale = Vector2(0, beam_length_scale)
 
 	var tween_scale = create_tween()
-	tween_scale.tween_property(self, "scale", Vector2(beam_width_scale, beam_length_scale), 2.0)
-	tween_scale.tween_property(self, "scale", Vector2(beam_width_active_scale, beam_length_scale), 0.1)
-	tween_scale.tween_property(self, "scale", Vector2(0, beam_length_scale), 0.1)
+	tween_scale.tween_property(self, "scale", Vector2(beam_width_scale, beam_length_scale), startup_time)
+	tween_scale.tween_property(self, "scale", Vector2(beam_width_active_scale, beam_length_scale), active_time)
+	tween_scale.tween_property(self, "scale", Vector2(0, beam_length_scale), disappear_time)
