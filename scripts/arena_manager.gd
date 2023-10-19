6169f53b472440e14308fae1abba4565109bf518
Lertos <deetasticgaming@gmail.com>
2023-10-19 07:59:47 -0300

    Removing unneeded values

--- scripts/arena_manager.gd
+++ scripts/arena_manager.gd
@@ -126,34 +126,6 @@ func spawn_event(padding: int, event_scene: PackedScene, parent_node: Node):
 	new_event.position = Vector2(x, y)
 
 
-func spawn_beam():
-	#Simply want to spawn it in the inner square, inside the ring of cannons
-	var padding = distance_from_walls * 2
-	var left_top_corner = Vector2(padding, padding)
-	var bottom_right_corner = Vector2(Config.arena_width - padding, Config.arena_height - padding)
-	
-	var x = rng.randi_range(left_top_corner.x, bottom_right_corner.x)
-	var y = rng.randi_range(left_top_corner.y, bottom_right_corner.y)
-	
-	var new_beam = SCENE_BEAM.instantiate()
-
-	$Beams.add_child(new_beam)
-	
-	new_beam.position = Vector2(x, y)
-
-
-func spawn_bomb():
-	pass
-
-
-func spawn_gravity_field():
-	pass
-
-
-func spawn_pull_field():
-	pass
-
-
 func spawn_special_multi_beam():
 	pass
 	
