63b80778a816dbde0a1fd715c240ffbcaf6c4ac7
Lertos <deetasticgaming@gmail.com>
2023-10-22 11:03:18 -0300

    Adding fading before object destruction

--- scenes/pull_field.gd
+++ scenes/pull_field.gd
@@ -16,4 +16,7 @@ func _on_area_2d_body_exited(body):
 
 
 func _on_timer_timeout():
+	#Fade before destroying the object
+	create_tween().tween_property(self, "modulate", Color.html("#ffffff00"), 1)
+	await get_tree().create_timer(1).timeout
 	queue_free()
