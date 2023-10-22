4cc3a1984407e0b0563ec8f117b08ec949ffe528
Lertos <deetasticgaming@gmail.com>
2023-10-22 09:57:30 -0300

    Adding logic to "upgrade" cannons

--- scenes/cannon.gd
+++ scenes/cannon.gd
@@ -3,10 +3,13 @@ extends StaticBody2D
 @onready var bullet_list = get_parent().get_parent().get_node("Bullets")
 
 var targeted_player = null
+var cannon_level: int = 1
+var max_cannon_level: int = 5
+var fire_rates: Array[float] = [1.0, 0.8, 0.6, 0.4, 0.2]
 
 
 func _ready():
-	pass
+	set_fire_rate()
 
 
 func _on_timer_timeout():
@@ -24,5 +27,25 @@ func _on_timer_timeout():
 	bullet_node.activate(targeted_player.position)
 
 
+func set_fire_rate():
+	$Timer.stop()
+	$Timer.wait_time = fire_rates[cannon_level - 1]
+	$Timer.start()
+
+
 func set_target(player: Node):
 	targeted_player = player
+	
+
+func upgrade_cannon():
+	if cannon_level + 1 > max_cannon_level:
+		return
+		
+	cannon_level += 1
+	
+	switch_cannon_asset()
+	set_fire_rate()
+
+
+func switch_cannon_asset():
+	$Sprite2D.texture = load("res://assets/cannon" + str(cannon_level) + ".png")
