6cb468a60021e0fd87ab30c84c7ac6f48b453d30
Lertos <deetasticgaming@gmail.com>
2023-11-03 20:47:24 -0300

    Adding skin names

--- scripts/global/game_manager.gd
+++ scripts/global/game_manager.gd
@@ -2,15 +2,15 @@ extends Node
 
 var skins = [
 	[preload("res://assets/player.png"), "Steve"],
-	[preload("res://assets/player2.png"), "Skin 1"],
-	[preload("res://assets/player3.png"), ""],
-	[preload("res://assets/player4.png"), ""],
-	[preload("res://assets/player5.png"), ""],
-	[preload("res://assets/player6.png"), ""],
-	[preload("res://assets/player7.png"), ""],
-	[preload("res://assets/player8.png"), ""],
-	[preload("res://assets/player9.png"), ""],
-	[preload("res://assets/player10.png"), ""]
+	[preload("res://assets/player2.png"), "Jared From Subway"],
+	[preload("res://assets/player3.png"), "Cyberjunk"],
+	[preload("res://assets/player4.png"), "Golden Boy"],
+	[preload("res://assets/player5.png"), "Mono Brown"],
+	[preload("res://assets/player6.png"), "Survivalist"],
+	[preload("res://assets/player7.png"), "Tim"],
+	[preload("res://assets/player8.png"), "King Kai"],
+	[preload("res://assets/player9.png"), "Kappa"],
+	[preload("res://assets/player10.png"), "Grandpa Casper"]
 ]
 
 var players = {}
